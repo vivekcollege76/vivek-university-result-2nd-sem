53f2ebfed8ccf91ec1099ef025d8a8fe70bfdfb5
vivekcollege76 <collegevivek627@gmail.com>
2026-07-18 11:34:01 +0530

    Add student data to data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const students = [
+
+    {
+        name: "manish",
+        roll: "11125091",
+        image: "images/manish.png"
+    },
+
+    {
+        name: "dipanshu",
+        roll: "11125092",
+        image: "images/dipanshu.png"
+    },
+
+    {
+        name: "rahul",
+        roll: "11125093",
+        image: "images/rahul.png"
+    }
+
+];
