46d120733d766a3d56b3525b5998667e3d72139e
vivekcollege76 <collegevivek627@gmail.com>
2026-07-19 11:04:09 +0530

    Add student data to data.js

---
+++ data.js
@@ -0,0 +1,70 @@
+const students = [
+
+    {
+        name: "manish kumar",
+        roll: "11125091",
+        image: "images/manish.png"
+    },
+
+    {
+        name: "dipanshu kumar",
+        roll: "11125043",
+        image: "images/dipanshu.png"
+    },
+
+    {
+        name: "riya",
+        roll: "11125023",
+        image: "images/riya.png"
+    },
+
+    {
+        name: "harshit chaudhary",
+        roll: "11125099",
+        image: "images/harshit chaudhary.png"
+    },
+
+    {
+        name: "harshit kumar",
+        roll: "11125079",
+        image: "images/harshit.png"
+    },
+
+    {
+        name: "krrish",
+        roll: "11125085",
+        image: "images/krrish.png"
+    },
+
+    {
+        name: "rohan saini",
+        roll: "11125100",
+        image: "images/rohan.png"
+    },
+
+    {
+        name: "vivek kumar",
+        roll: "11125066",
+        image: "images/vivek.png"
+    },
+
+    {
+        name: "ayush chauhan",
+        roll: "11125092",
+        image: "images/ayush.png"
+    },
+
+    {
+        name: "lalit kumar",
+        roll: "11125067",
+        image: "images/lalit.png"
+    },
+
+    {
+        name: "kartik kumar",
+        roll: "11125036",
+        image: "images/kartik.png"
+
+    }   
+
+];
